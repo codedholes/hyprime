8d1f06e30d682ad80fa809d21fc745113d98db75
codedholes <codedholes@gmail.com>
2017-02-18 19:45:45 +0100

    M001b

--- Hyprime/Properties/AssemblyInfo.cs
+++ Hyprime/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hyprime")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An executable for finding Mersenne-primes.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("codedHoles")]
 [assembly: AssemblyProduct("Hyprime")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("M001a")]
+[assembly: AssemblyFileVersion("M001a")]
